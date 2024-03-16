76d30956fcd562f7e277a89cd300d7e473c682ec
Dominik Schwind <dschwind@lostfocus.de>
2024-03-16 11:31:45 +0100

    asserInternal has been removed

--- tests/Scraper/RulesTest.php
+++ tests/Scraper/RulesTest.php
@@ -8,11 +8,11 @@ class RulesTest extends \PHPUnit\Framework\TestCase
     public function testThatRulesAreValid()
     {
         foreach (glob(__DIR__.'/../../lib/PicoFeed/Rules/.*.php') as $filename) {
-            $this->assertInternalType('array', include($filename), 'Rule invalid: '.$filename);
+            $this->assertIsArray(include($filename), 'Rule invalid: '.$filename);
         }
 
         foreach (glob(__DIR__.'/../../lib/PicoFeed/Rules/*.php') as $filename) {
-            $this->assertInternalType('array', include($filename), 'Rule invalid: '.$filename);
+            $this->assertIsArray(include($filename), 'Rule invalid: '.$filename);
         }
     }
 }
